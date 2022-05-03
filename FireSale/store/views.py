db6802bb1d2316bf9f9963b4a41bd9bfa77e0d0e
axele21 <axele21@ru.is>
2022-05-03 16:20:18 +0000

    Payment, sellproduct, itemDetails html files added

--- FireSale/store/views.py
+++ FireSale/store/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
-def item(request):
-        return render(request,'Store.')
+
+def itemDetails(request):
+    return render(request, 'store/product/itemDetails.html')
+
+def sellProduct(request):
+    return render(request, 'store/product/sell.html')
+
+def pay(request):
+    return render(request, 'store/payment/pay.html')
