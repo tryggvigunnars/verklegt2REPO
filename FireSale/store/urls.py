0ca8f0af0337e03fb7694cbc618db3f3558767a4
axele21 <axele21@ru.is>
2022-05-03 16:26:48 +0000

    payment, sellproducts and item details html added

---
+++ FireSale/store/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('itemDetails/', views.itemDetails, name="itemDetails"),
+    path('sellProduct/', views.sellProduct, name="sellProduct"),
+    path('payment/', views.pay, name="payment")
+]
