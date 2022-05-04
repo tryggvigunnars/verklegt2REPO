8941e690b23d3c6e559280e341fab26691ed3f0b
axele21 <89972357+axele21@users.noreply.github.com>
2022-05-04 15:05:22 +0000

    navigation between urls added

--- FireSale/account/urls.py
+++ FireSale/account/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.login, name="login"),
     path('register/', views.register, name="register"),
     path('profile/', views.profile, name='profile'),
-    path('editprofile/', views.editprofile, name='editprofile'),
+    path('editprofile/', views.editprofile, name='editProfile'),
 ]
