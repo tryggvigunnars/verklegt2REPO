132c98971484fd94d5794cd6f6e57c985b47538c
axele21 <89972357+axele21@users.noreply.github.com>
2022-05-04 12:59:48 +0000

    dummy data for item added

--- FireSale/store/views.py
+++ FireSale/store/views.py
@@ -3,11 +3,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
+item_info = [
+    {'img': '', 'productName': 'Bike', 'seller': 'Kalli', 'location': 'Reykjavík', 'price': 3000},
+    {'img': '', 'productName': 'Car', 'seller': 'Páll', 'location': 'Reykjavík', 'price': 5000},
+    {'img': '', 'productName': 'Scooter', 'seller': 'Nilli', 'location': 'Reykjavík', 'price': 80000}
+]
+
 def item(request):
-        return render(request,'store/product/item.html')
+        return render(request,'store/product/item.html', context={'item_info': item_info})
 
 def itemDetails(request):
     return render(request, 'store/product/itemDetails.html')
+
 
 def sellProduct(request):
     return render(request, 'store/product/sell.html')
