ae63a691fc3f8a480b30bb463749894bb4c210c6
kristinngg99 <kristinngg99@gmail.com>
2022-05-09 12:08:18 +0000

    working on athentication

--- FireSale/account/urls.py
+++ FireSale/account/urls.py
@@ -1,12 +1,16 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name="login"),
+    path('login', LoginView.as_view(template_name='Account/login.html'), name='login'),
+    path('logout', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', views.register, name="register"),
-    path('profile/', views.profile, name='profile'),
 
+    path('profile/', views.profile, name='profile'),
     path('editprofile/', views.editprofile, name='editprofile'),
+
     path('myListings/', views.myListings, name="myListings"),
     path('myBids/', views.myBids, name="myBids"),
+
 ]
--- FireSale/account/views.py
+++ FireSale/account/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+
+from django.contrib. auth. forms import UserCreationForm
+from django. shortcuts import render, redirect
 from django.http import HttpResponse
 # Create your views here.
 
@@ -10,7 +12,14 @@ def login(request):
     return render(request, 'Account/login.html')
 
 def register(request):
-    return render(request, 'Account/register.html')
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    return render(request, 'Account/register2.html', {
+        'form': UserCreationForm()
+    })
 
 def profile(request):
     return render(request, 'Account/profile.html')
@@ -23,3 +32,6 @@ def myListings(request):
 
 def myBids(request):
     return render(request, 'account/myBids.html')
+
+
+
